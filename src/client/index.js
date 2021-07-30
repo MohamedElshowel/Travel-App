067e2f39ee26017a1cd0a523ca4b9311013da95b
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2021-07-30 17:52:25 +0200

    feat: Add functionality for generating weather forecast using `addEventListener`

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,9 @@ import { generateWeatherJournal } from "./js/app";
 
 import './styles/styles.scss';
 
+// Add event listener on the click of "Generate" button.
+document.getElementById('generate').addEventListener('click', generateWeatherJournal);
+
 export {
     generateWeatherJournal,
 }
