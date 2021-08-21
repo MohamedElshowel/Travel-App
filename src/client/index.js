f825ebb30f88de6bc73ec63c8e3b1baa99b8dbf8
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2021-08-21 07:56:46 +0200

    feat: Add min & max values for departure date picker matched with weather forecast API

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,18 @@ import { generateWeatherJournal } from "./js/app";
 
 import './styles/styles.scss';
 
-// Add event listener on the click of "Generate" button.
-document.getElementById('generate').addEventListener('click', generateWeatherJournal);
+(() => {
+    // Add event listener on the click of "Generate" button.
+    document.getElementById('generate').addEventListener('click', generateWeatherJournal);
+    // Add a minimum and maximum dates for departure date picker.
+    const datePickerElement = document.getElementById('datePicker');
+    const timeNow = new Date();
+    const todayDate = timeNow.toISOString().slice(0, 10);
+    datePickerElement.setAttribute('min', todayDate);
+    datePickerElement.setAttribute('value', todayDate);
+    const maximumDate = new Date(timeNow.setDate(timeNow.getDate() + 16)).toISOString().slice(0, 10);
+    datePickerElement.setAttribute('max', maximumDate);
+})();
 
 export {
     generateWeatherJournal,
